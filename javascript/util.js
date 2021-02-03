7b8d5dc6fe8a9c5536e3a881c502ba5ed29f93a0
skea <253590433@qq.com>
2021-02-03 14:48:04 +0800

    add isArray method

--- javascript/util.js
+++ javascript/util.js
@@ -15,8 +15,16 @@ function isRepeatArr(arr) {
   }
   return false
 }
+function isArray(value) {
+    if (typeof Array.isArray === "function") {
+      return Array.isArray(value);
+    } else {
+      return Object.prototype.toString.call(value) === "[object Array]";
+    }
+}
 
 module.exports = {
   isEmptyObj,
-  isRepeatArr
+  isRepeatArr,
+  isArray
 }
